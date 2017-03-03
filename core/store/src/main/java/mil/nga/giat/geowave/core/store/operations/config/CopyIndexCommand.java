97ca15e1adf493a3340d8e52a66170e4e63b515c
Rebecca <reb343@cornell.edu>
2017-03-03 11:30:33 -0500

    cpindex endpoint

--- core/store/src/main/java/mil/nga/giat/geowave/core/store/operations/config/CopyIndexCommand.java
+++ core/store/src/main/java/mil/nga/giat/geowave/core/store/operations/config/CopyIndexCommand.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+// new code
+import org.apache.commons.lang3.StringUtils;
+import org.restlet.resource.Get;
+// end new code
+
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
 import com.beust.jcommander.Parameters;
@@ -16,12 +21,18 @@ import mil.nga.giat.geowave.core.cli.api.OperationParams;
 import mil.nga.giat.geowave.core.cli.operations.config.ConfigSection;
 import mil.nga.giat.geowave.core.cli.operations.config.options.ConfigOptions;
 import mil.nga.giat.geowave.core.store.operations.remote.options.IndexPluginOptions;
+import mil.nga.giat.geowave.core.cli.parser.ManualOperationParams;
 
 @GeowaveOperation(name = "cpindex", parentOperation = ConfigSection.class)
 @Parameters(commandDescription = "Copy and modify existing index configuration")
 public class CopyIndexCommand implements
 		Command
 {
+	// new code
+	private static int SUCCESS = 0;
+	private static int USAGE_ERROR = -1;
+	private static int INDEX_EXISTS = -2;
+	// end new code
 
 	@Parameter(description = "<name> <new name>")
 	private List<String> parameters = new ArrayList<String>();
@@ -68,22 +79,73 @@ public class CopyIndexCommand implements
 	public void execute(
 			OperationParams params ) {
 
+		String result = computeResults(params);
+
+	}
+
+	@Get("json")
+	public String computeResults(
+			OperationParams params ) {
+		// TODO
+		String key = getQueryValue("key");
+		String value = getQueryValue("value");
+
+		if ((key == null || key.equals("")) || value == null) {
+			return "{ \"result\":" + USAGE_ERROR
+					+ ", \"message\":\"Must specify existing index and new index (comma separated)\",\"prev\":\"\"}";
+		}
+
+		setParameters(
+				key,
+				value);
+		// OperationParams params = new ManualOperationParams();
+		// TODO just adding this file information causes the config file to
+		// be stored as 'unknownversion-config.properties' which probably
+		// should change..
+		params.getContext().put(
+				ConfigOptions.PROPERTIES_FILE_CONTEXT,
+				ConfigOptions.getDefaultPropertyFile());
+
+		Result result = copyIndex(params);
+
+		if (result.result == USAGE_ERROR) {
+			return "{ \"result\":" + USAGE_ERROR + ", \"message\":\"usage error\":\"" + "\"}";
+		}
+		else if (result.result == INDEX_EXISTS) {
+			return "{ \"result\":" + INDEX_EXISTS + ", \"message\":\"index exists error\",\"indexname\":\""
+					+ result.newIndex + "\"}";
+		}
+		else {
+			return "{ \"result\":" + SUCCESS + ", \"message\":\"\",\"indexname\":\"" + result.newIndex + "\"}";
+		}
+	}
+
+	/* TODO */
+	private Result copyIndex(
+			OperationParams params ) {
+		Result result = new Result();
+
 		if (parameters.size() < 2) {
-			throw new ParameterException(
-					"Must specify <existing index> <new index> names");
+			// throw new ParameterException(
+			// "Must specify <existing index> <new index> names");
+			result.result = USAGE_ERROR;
+			return result;
 		}
 
 		// This is the new index name.
 		String newIndex = parameters.get(1);
 		String newIndexNamespace = IndexPluginOptions.getIndexNamespace(newIndex);
+		result.newIndex = newIndex;
 
 		// Make sure we're not already in the index.
 		IndexPluginOptions existPlugin = new IndexPluginOptions();
 		if (existPlugin.load(
 				existingProps,
 				newIndexNamespace)) {
-			throw new ParameterException(
-					"That index already exists: " + newIndex);
+			// throw new ParameterException(
+			// "That index already exists: " + newIndex);
+			result.result = INDEX_EXISTS;
+			return result;
 		}
 
 		// Save the options.
@@ -103,6 +165,8 @@ public class CopyIndexCommand implements
 				configFile,
 				existingProps);
 
+		result.result = SUCCESS;
+		return result;
 	}
 
 	public List<String> getParameters() {
@@ -116,5 +180,13 @@ public class CopyIndexCommand implements
 		this.parameters.add(existingIndex);
 		this.parameters.add(newIndex);
 	}
+
+	// new code
+	private static class Result
+	{
+		int result;
+		String newIndex; // TODO update for this class
+	}
+	// end new code
 
 }
