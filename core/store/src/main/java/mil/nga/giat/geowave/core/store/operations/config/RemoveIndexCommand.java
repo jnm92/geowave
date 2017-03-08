4f4445a2d3825260dcc5d4cfd620234c9625c9f7
Kevin <mr.kev@me.com>
2017-03-08 03:09:42 -0500

    add geowave/config/rmindex

--- core/store/src/main/java/mil/nga/giat/geowave/core/store/operations/config/RemoveIndexCommand.java
+++ core/store/src/main/java/mil/nga/giat/geowave/core/store/operations/config/RemoveIndexCommand.java
@@ -1,12 +1,22 @@
 package mil.nga.giat.geowave.core.store.operations.config;
 
-import com.beust.jcommander.Parameters;
 
+import com.beust.jcommander.Parameters;
 import mil.nga.giat.geowave.core.cli.annotations.GeowaveOperation;
 import mil.nga.giat.geowave.core.cli.api.Command;
 import mil.nga.giat.geowave.core.cli.api.OperationParams;
 import mil.nga.giat.geowave.core.cli.operations.config.ConfigSection;
+import mil.nga.giat.geowave.core.cli.operations.config.options.ConfigOptions;
+import mil.nga.giat.geowave.core.cli.parser.ManualOperationParams;
 import mil.nga.giat.geowave.core.store.operations.remote.options.IndexPluginOptions;
+import org.restlet.data.Status;
+import org.restlet.resource.Delete;
+import org.restlet.resource.Post;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
 
 @GeowaveOperation(name = "rmindex", parentOperation = ConfigSection.class)
 @Parameters(commandDescription = "Remove index configuration from Geowave configuration")
@@ -15,8 +25,8 @@ public class RemoveIndexCommand extends
 		Command
 {
 
-	@Override
-	public void execute(
+
+	public void computeResults(
 			OperationParams params ) {
 
 		// Search for properties relevant to the given name
@@ -27,4 +37,29 @@ public class RemoveIndexCommand extends
 
 	}
 
+	@Override
+	public void execute(OperationParams params ) {
+		computeResults(params);
+	}
+
+
+	@Delete("json")
+	public void restPost() {
+
+		String pattern = getQueryValue("pattern");
+
+		if (pattern == null) {
+			this.setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+			return;
+		}
+
+		this.setEntryName(pattern);
+
+		OperationParams params = new ManualOperationParams();
+		params.getContext().put(
+				ConfigOptions.PROPERTIES_FILE_CONTEXT,
+				ConfigOptions.getDefaultPropertyFile()
+		);
+		computeResults(params);
+	}
 }
